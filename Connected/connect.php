8be1145167874ebc5e4dad32b808b24a6c2039af
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-02-04 18:13:24 +0700

    Test

---
+++ Connected/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "intranet";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, "electric");
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
--- auth/check_login.php
+++ auth/check_login.php
@@ -1,6 +1,13 @@
+<? require_once('../Connected/connect.php') ?>
 <?php
 if ($_POST) {
     echo $_POST['username'];
+    $data = mysqli_query($conn,"select * from data");
+    print_r($data);
+    echo "<br/>";
+    while($row = mysqli_fetch_assoc($data)){
+        echo $row['fname'],'<br/>';
+    }
     exit(0);
 }else{
     header("Location: login.php");
