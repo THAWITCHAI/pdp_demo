2ccc8051c782be12fe4fcb1bd93d0b2951a06ad7
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-02-04 17:20:05 +0700

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -8,29 +8,35 @@
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
 </head>
 <style>
-    *{
+    * {
         font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size: 14px;
     }
 </style>
 
-<body class="flex flex-col justify-start pt-10 items-center gap-2 w-full min-h-screen">
-    <div>
+<body class="flex flex-col justify-start pt-10 items-center gap-2 w-full min-h-screen bg-gray-200">
+    <div class="text-blue-500 font-light text-[35px]">
         PDP System
     </div>
     <h1>release:DEV.2.10</h1>
-    <div class="bg-gray-50 shadow-sm w-1/4">
-        <div class="w-full flex">
-            <button class="shadow-sm w-full">Staff</button>
-            <button class="shadow-sm w-full">Doctor</button>
-        </div>
-        <div class="w-full p-5 flex flex-col gap-4 justify-center items-center">
-            <h1 class="w-full">ระบบสำหรับเจ้าหน้าที่ <hr></h1>
-            <div class="flex flex-col gap-2 w-full">
-                <input class="border w-full h-[2rem] text-[16px] px-2  rounded-sm" type="text" placeholder="Username">
-                <input class="border w-full h-[2rem] text-[16px]  px-2 rounded-sm" type="text" placeholder="Password">
+    <div class="w-1/4 p-[1px] bg-blue-500">
+        <form action="" method="post" class="bg-gray-50 shadow-sm w-full p-[1px]">
+            <div class="w-full h-fit gap-[4px] flex">
+                <button class="shadow-sm cursor-pointer w-full text-start text-[14px] bg-white p-2">Staff</button>
+                <button class="shadow-sm cursor-pointer w-full text-start text-[14px] p-2">Doctor</button>
             </div>
-        </div>
+            <div class="w-full p-5 flex flex-col gap-4 justify-center items-center">
+                <h1 class="w-full">
+                    <p class="text-lg text-blue-500 border-b border-gray-200">ระบบสำหรับเจ้าหน้าที่</p>
+                </h1>
+                <div class="flex flex-col gap-2 w-full">
+                    <input required name="username" class="bg-white outline-none ring ring-gray-200  w-full h-[2rem] text-[14px] px-2  rounded-sm" type="text" placeholder="Username">
+                    <input required name="password" class="bg-white outline-none ring ring-gray-200 w-full h-[2rem] text-[14px]  px-2 rounded-sm" type="password" placeholder="Password">
+                    <div class="w-full p-2 flex justify-end items-center">
+                        <button type="submit" class="px-4 py-1 text-white rounded-xs cursor-pointer bg-blue-500 w-1/4">Login</button>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </body>
 
