9c08aa0894293dd385a77c542b90c05c50f63d72
Thawitchai Boonsong <thawitchai285@gmail.com>
2025-02-04 18:31:58 +0700

    Check_login

--- auth/check_login.php
+++ auth/check_login.php
@@ -1,14 +1,22 @@
 <? require_once('../Connected/connect.php') ?>
 <?php
 if ($_POST) {
-    echo $_POST['username'];
-    $data = mysqli_query($conn,"select * from data");
-    print_r($data);
-    echo "<br/>";
-    while($row = mysqli_fetch_assoc($data)){
-        echo $row['fname'],'<br/>';
+    // echo $_POST['username'];
+    $data = mysqli_query($conn, "select * from data");
+    // print_r($data);
+    // echo "<br/>";
+    $check_username = false;
+    while ($row = mysqli_fetch_assoc($data)) {
+        if ($_POST['username'] == $row['fname']) {
+            $check_username = true;
+            break;
+        }
     }
+    if ($check_username == false) {
+        header("Location: login.php");
+    }
+    echo "Ok";
     exit(0);
-}else{
+} else {
     header("Location: login.php");
 }
